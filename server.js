674e89d19f5433c22d5601a7b6dba5bc60bd231f
Shreya Srinivas <srinivas.shreya@gmail.com>
2015-02-07 16:05:14 -0500

    basic node server

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+//Visit localhost:8080
+app.listen(8080, function() { console.log(' - listening on port 8080');});
