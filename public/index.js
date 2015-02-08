9983b50d1984973a5c0ada07876bee97acb423a6
sch3 <steven_ho@brown.edu>
2015-02-08 09:20:33 -0500

    Misc Fixes for index.js
    Added a fix for the vdub being closed on the weekends. Also, tried
    modify the global variables.

--- public/index.js
+++ public/index.js
@@ -13,18 +13,18 @@ var closed;
 var rattyfinalres=[];
 var vdubfinalres=[];
 //test methods
-// $(window).load(function() {
-// console.log("here");
-//         // $("#hello").click(function() {
+$(window).load(function() {
+console.log("here");
+         //$("#hello").click(function() {
 
-//         //     getVdub();
-//         // });
+         getVdub();
+         //});
 
 //     getRatty();
 //     console.log(length(rattyfinalres[0]));
 //     getVdub();
 
-// });
+});
   
 function getServerTime() {
     var date = new Date();
@@ -40,43 +40,58 @@ function getVdub(){
     var date = getServerTime();
     var hour = date.getHours();
     var minutes = date.getMinutes();
-    closed = false;
+    var closed = false;
     var tomorrow = false;
     var earlybreakfast = false;
     var closingsoon;
-    
-    if((hour<7)||((hour===7)&&(minutes<30))){
+    var day = date.getDay();
+    var monthday = date.getDate();
+    //vdub closes over the weekend
+    if(day===6||day===0){
+        console.log("weekend");
+        if(day===6){
+            baseurl = baseurl +"&year="+date.getFullYear()+"&month="+(date.getMonth()+1)+"&day="+(monthday+2)+"&hour=8";
+        }
+        else{
+            baseurl = baseurl +"&year="+date.getFullYear()+"&month="+(date.getMonth()+1)+"&day="+(monthday+1)+"&hour=8";
+        }
         closed = true;
-        earlybreakfast = true;
     }
-    if((hour>19)||((hour===19)&&(minutes>30))){
-        closed = true;
-        tomorrow = true;
+    else{
+        if((hour<7)||((hour===7)&&(minutes<30))){
+            closed = true;
+            earlybreakfast = true;
+        }
+        if((hour>19)||((hour===19)&&(minutes>30))){
+            closed = true;
+            tomorrow = true;
+        }
     }
     //vdub is closed between 2:30 -4:30 pm. This deals with that edge case. Gets next meal, which is dinner that day.
     var isgreater = (hour>14)||((hour===14)&&(minutes>30));
     var islesser = (hour>16)||((hour===16)&&(minutes<30));
     if(isgreater&&islesser){
         closed= true;
-        baseurl = baseurl +"&year="+date.getFullYear()+"&month="+(date.getMonth()+1)+"&day="+(date.getDay()+1)+"&hour=5";
+        baseurl = baseurl +"&year="+date.getFullYear()+"&month="+(date.getMonth()+1)+"&day="+(monthday+1)+"&hour=5";
     }
     console.log(closed);
     //get tomorrow's menu later, change url now.
     if(tomorrow){
-        baseurl = baseurl +"&year="+date.getFullYear()+"&month="+(date.getMonth()+1)+"&day="+(date.getDay()+1)+"&hour=8";
+        baseurl = baseurl +"&year="+date.getFullYear()+"&month="+(date.getMonth()+1)+"&day="+(monthday+1)+"&hour=8";
     }
-    //if early on a day before vdub opens, get next day's menu
+    //if early on a day before vdub opens, get next menu for that day
     if(earlybreakfast){
-        baseurl = baseurl +"&year="+date.getFullYear()+"&month="+(date.getMonth()+1)+"&day="+(date.getDay())+"&hour=8";
+        baseurl = baseurl +"&year="+date.getFullYear()+"&month="+(date.getMonth()+1)+"&day="+(day)+"&hour=8";
     }
-    closingsoon =hour===19&&minutes<30;
+    //if less than thirty minutes from closing time
+    closingsoon = (hour===19&&minutes<30);
     if(closingsoon){
         closingsoon = "Closing Soon";
     }
     else{
         closingsoon = "Not Closing Soon";
     }
-    var vdubesults = [];  
+    var vdubresults = [];  
     $.ajax( {
             url: baseurl,
             dataType: "jsonp",
@@ -88,11 +103,11 @@ function getVdub(){
                 var allmenus = data.menus[0];
                 var dailysbar = allmenus["daily sidebars"];
                 var mainmenu = allmenus["main menu"];
-                results.push(dailysbar);
-                results.push(mainmenu);
-                console.log(results);
-                vdubfinalres = results;
-                return results;
+                vdubresults.push(dailysbar);
+                vdubresults.push(mainmenu);
+                console.log(vdubresults);
+                vdubfinalres = vdubresults;
+                return vdubresults;
             }
         });
     
@@ -107,17 +122,18 @@ function getRatty(){
     var minutes = date.getMinutes();
     //if greater than 7:30 pm, then display tomorrow's menu
     var tomorrow = false;
+    var dayofmonth = date.getDate();
     closed = false;
     if((hour==19&&minutes>30)||hour>19){
         closed = true;
        //get tomorrow morning's menu
-       baseurl = baseurl +"&year="+date.getFullYear()+"&month="+(date.getMonth()+1)+"&day="+(date.getDay()+1)+"&hour=10";
+       baseurl = baseurl +"&year="+date.getFullYear()+"&month="+(date.getMonth()+1)+"&day="+(dayofmonth+1)+"&hour=10";
         console.log(baseurl);
         tomorrow = true;
     }
     //somehow, really early morning, display breakfast of that day
     if(hour<7||(hour==7&&minutes<30)){
-        baseurl = baseurl +"&year="+date.getFullYear()+"&month="+(date.getMonth()+1)+"&day="+(date.getDay())+"&hour=10";
+        baseurl = baseurl +"&year="+date.getFullYear()+"&month="+(date.getMonth()+1)+"&day="+(dayofmonth)+"&hour=10";
    }
     console.log(date+" "+hour+" "+minutes+" "+(date.getMonth()));
     if(hour===19&&minutes<30){
