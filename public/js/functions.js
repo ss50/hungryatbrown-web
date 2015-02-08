ce899c9e2909ed23de4a7e01761df1a0dbfb39be
Annie Cheng <ac962@cornell.edu>
2015-02-08 02:59:59 -0500

    Updated UI

--- public/js/functions.js
+++ public/js/functions.js
@@ -2,14 +2,17 @@ function toggleNav() {
     var menu = document.getElementById("menu-button"), current_class = menu.className;
     var nav = document.getElementById("nav-container");
     var list = document.getElementById("container");
+    var bg = document.getElementById("background");
     
     menu.className = (current_class == "open") ? "" : "open";
 
     if (nav.className != "nav-opened") {
         nav.className = "nav-opened";
+        bg.className = "nav-opened";
         list.className = "nav-opened";
     } else {
         nav.className = "";
+        bg.className = "";
         list.className = "";
     }
 }
